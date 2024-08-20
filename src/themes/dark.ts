078ec7a61b21782277f1b8bca17f5182ede3d07d
Álex Maciel <aweasley.gina@gmail.com>
2024-08-20 15:14:10 -0300

    feat: criando thema e contexto do thema

---
+++ src/themes/dark.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material'
+
+
+export const DarkTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#FFFFFF',
+        },
+        background: {
+            default: '#000000',
+        },
+    }
+})
---
+++ src/themes/light.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material'
+
+export const LightTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#FF4500',
+        },
+        background: {
+            default: '#D9D9D9',
+        }
+    }
+})
